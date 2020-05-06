1125b8167056b51dfe9723efea3d4abccfd343a7
大倉祥一 <okurashoichi@team-lab.com>
2020-05-07 07:26:56 +0900

    記事登録更新削除機能

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,4 +11,6 @@
 |
 */
 Auth::routes();
-Route::get('/', 'ArticleController@index');
+Route::get('/', 'ArticleController@index')->name('articles.index');;
+Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->only(['show']);
