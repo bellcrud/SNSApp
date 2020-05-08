eaafd38158a00f697f40a6c3eaf765c9d2af88ad
大倉祥一 <okurashoichi@team-lab.com>
2020-05-09 08:38:31 +0900

    いいね機能

--- laravel/resources/views/articles/card.blade.php
+++ laravel/resources/views/articles/card.blade.php
@@ -53,14 +53,25 @@
         @endif
 
     </div>
-    <div class="card-body pt-0">
+    <div class="card-body pt-0 pb-2">
         <h3 class="h4 card-title">
             <a class="text-dark" href="{{ route('articles.show', ['article' => $article]) }}">
                 {{ $article->title }}
             </a>
         </h3>
         <div class="card-text">
-            {{ $article->body }}
+            {!! nl2br(e( $article->body )) !!}
+        </div>
+    </div>
+    <div class="card-body pt-0 pb-2 pl-3">
+        <div class="card-text">
+            <article-like
+                    :initial-is-liked-by='@json($article->isLikedBy(Auth::user()))'
+                    :initial-count-likes='@json($article->count_likes)'
+                    :authorized='@json(Auth::check())'
+                    endpoint="{{ route('articles.like', ['article' => $article]) }}"
+            >
+            </article-like>
         </div>
     </div>
 </div>
